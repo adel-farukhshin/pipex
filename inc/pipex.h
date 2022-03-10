680e1ded974b3d3647a7f407febda97061196b4d
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-10 15:29:49 +0300

    refactor: change **env to **path

--- inc/pipex.h
+++ inc/pipex.h
@@ -29,10 +29,16 @@ typedef struct s_pipex
 {
 	int		argc;
 	char	**argv;
-	char	**env;
+	char	**path;
 }	t_pipex;
 
-int		connect(void);
-char	*ft_strstr(const char *haystack, const char *needle);
+void			get_path(char **env);
+
+int				connect(void);
+char			*ft_strstr(const char *haystack, const char *needle);
+int				ft_strncopy(char *dst, const char *src, int n);
+unsigned long	ft_strlen(const char *s);
+char			*ft_strdup(const char *s1);
+char			**ft_split(char const *s, char c);
 
 #endif

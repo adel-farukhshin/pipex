0f62cf235dc5c901a651d4b6b143f5482c8af3ea
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-10 13:22:09 +0300

    feat: add pipe layout

--- src/pipex.c
+++ src/pipex.c
@@ -10,10 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#include <stdlib.h>
+#include <time.h>
+
 #include "ft_printf.h"
 
 int	main()
 {
-	ft_printf("Hello\n");
+	int	fd[2];
+
+	if (pipe(fd) == -1)
+	{
+		return (1);
+	}
+
+
+	int	pid1 = fork();
+	
+	if (pid1 < 0)
+	{
+		return (2);
+	}
+	if (pid1 == 0)
+	{ 
+		// ft_printf("In child 1\n");
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		close(fd[0]);
+		execlp("ping", "ping", "-c", "1", "google.com", NULL);
+	}
+
+	int	pid2 = fork();
+	if (pid2 < 0)
+	{
+		return (3);
+	}
+	if (pid2 == 0)
+	{
+		// ft_printf("In child 1\n");
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "PING", NULL);
+	}
+	// ft_printf("Hi\n");
+
+	close(fd[0]); 
+	close(fd[1]); 
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2 , NULL, 0);
+	
 	return (0);
 }

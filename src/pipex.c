956513c96e7b5e237be80fce9631b8f74592bc30
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-10 13:28:21 +0300

    fix: add file handle

--- src/pipex.c
+++ src/pipex.c
@@ -38,11 +38,17 @@ int	main()
 	}
 	if (pid1 == 0)
 	{ 
+		int	file = open("in", O_RDONLY);
+		if (file == -1)
+			return (4);
+		dup2(file, STDIN_FILENO);
+		close(file);
 		// ft_printf("In child 1\n");
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
 		close(fd[0]);
-		execlp("ping", "ping", "-c", "1", "google.com", NULL);
+		execlp("cat", "cat", NULL);
+		// execlp("ping", "ping", "-c", "1", "google.com", NULL);
 	}
 
 	int	pid2 = fork();
@@ -52,11 +58,13 @@ int	main()
 	}
 	if (pid2 == 0)
 	{
+		int	file = open("out", O_CREAT | O_WRONLY, 0777);
 		// ft_printf("In child 1\n");
 		dup2(fd[0], STDIN_FILENO);
+		dup2(file, STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
-		execlp("grep", "grep", "PING", NULL);
+		execlp("grep", "grep", "", NULL);
 	}
 	// ft_printf("Hi\n");
 

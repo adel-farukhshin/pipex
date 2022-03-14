a06c7d9eb0deb456b8231a3f6eec210bdde9c8f7
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-14 14:02:52 +0300

    feat: add init_pipex function; free pipex structure in case of error of malloc p.cmd

--- src/pipex.c
+++ src/pipex.c
@@ -22,6 +22,22 @@
 #include "ft_printf.h"
 #include "pipex.h"
 
+void	init_pipex(t_pipex *pipex, int argc, char **argv, char **env)
+{
+	pipex->cmd = malloc(sizeof(char *) * 3);
+	if (!pipex->cmd)
+	{
+		free(pipex);
+		exit (-1);
+	}
+	pipex->cmd[0] = NULL;
+	pipex->cmd[1] = NULL;
+	pipex->cmd[2] = NULL;
+	pipex->argc = argc;
+	pipex->argv = argv;
+	pipex->env = env;
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	t_pipex	*pipex;
@@ -40,25 +56,17 @@ int	main(int argc, char **argv, char **env)
 		exit(-1);
 	}
 	close(fd);
-		pipex = malloc(sizeof(t_pipex));
-		if (pipex == NULL)
-			return (-1);
-		pipex->cmd = malloc(sizeof(char *) * 3);
-		if (!pipex->cmd)
-			return (-1);
-		pipex->cmd[0] = NULL;
-		pipex->cmd[1] = NULL;
-		pipex->cmd[2] = NULL;
-		pipex->argc = argc;
-		pipex->argv = argv;
-		pipex->env = env;
-		get_path(pipex);
-		get_cmd(pipex);
-		connect(pipex);
-		free_pipex(pipex);
-		// sleep(100);
-		// ft_free_words(pipex->path);
-	
+	pipex = malloc(sizeof(t_pipex));
+	if (pipex == NULL)
+		return (-1);
+	init_pipex(pipex, argc, argv, env);
+	get_path(pipex);
+	get_cmd(pipex);
+	connect(pipex);
+	free_pipex(pipex);
+	// sleep(100);
+	// ft_free_words(pipex->path);
+
 	
 	return (0);
 }

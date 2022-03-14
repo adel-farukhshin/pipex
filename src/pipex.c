9fe72fb61f182e2e07ad64fae845cdba2f128cc6
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-14 13:54:27 +0300

    refactor: check for ability to open the file

--- src/pipex.c
+++ src/pipex.c
@@ -10,7 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> 
+#include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
@@ -24,17 +25,27 @@
 int	main(int argc, char **argv, char **env)
 {
 	t_pipex	*pipex;
-	// int	fd[2];
+	int	fd;
 
 	if (argc != 5)
 	{
 		write(2, "Error: ./pipex file1 cmd1 cmd2 file2\n", 37);
 		exit(1);
 	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		perror("Error message");
+		exit(-1);
+	}
+	close(fd);
 		pipex = malloc(sizeof(t_pipex));
 		if (pipex == NULL)
-			return (1);
+			return (-1);
 		pipex->cmd = malloc(sizeof(char *) * 3);
+		if (!pipex->cmd)
+			return (-1);
 		pipex->cmd[0] = NULL;
 		pipex->cmd[1] = NULL;
 		pipex->cmd[2] = NULL;
@@ -45,6 +56,7 @@ int	main(int argc, char **argv, char **env)
 		get_cmd(pipex);
 		connect(pipex);
 		free_pipex(pipex);
+		// sleep(100);
 		// ft_free_words(pipex->path);
 	
 	

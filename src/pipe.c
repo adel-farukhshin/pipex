cd93a7420ea55ab5c8edf3c8f4a5236ab14848fc
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-14 14:34:33 +0300

    style: norm

--- src/pipe.c
+++ src/pipe.c
@@ -43,18 +43,12 @@ int	child1(int fd[2], t_pipex *pipex)
 	int		file;	
 	int		pid1;
 	char	**l_argv;
-	
+
 	pid1 = fork_child(fd);
 	if (pid1 < 0)
 		return (-1);
-	// pid1 = fork();
-	// if (pid1 < 0)
-	// {
-	// 	err_process(fd);
-	// 	return (-1);
-	// }
 	if (pid1 == 0)
-	{ 
+	{
 		file = open(pipex->argv[1], O_RDONLY);
 		if (file == -1)
 		{
@@ -69,7 +63,7 @@ int	child1(int fd[2], t_pipex *pipex)
 		l_argv = ft_split(pipex->argv[2], ' ');
 		execve(pipex->cmd[0], l_argv, pipex->env);
 	}
-	return(pid1);
+	return (pid1);
 }
 
 int	child2(int fd[2], t_pipex *pipex)
@@ -81,18 +75,11 @@ int	child2(int fd[2], t_pipex *pipex)
 	pid2 = fork_child(fd);
 	if (pid2 < 0)
 		return (-1);
-
-	// pid2 = fork();
-	// if (pid2 < 0)
-	// {
-	// 	err_process(fd);
-	// 	return (-1);
-	// }
 	if (pid2 == 0)
 	{
 		unlink(pipex->argv[pipex->argc - 1]);
-		file = open(pipex->argv[pipex->argc - 1]
-				, O_WRONLY | O_CREAT, 0777);
+		file = open(pipex->argv[pipex->argc - 1],
+				O_WRONLY | O_CREAT, 0777);
 		if (file < 0)
 		{
 			err_process(fd);
@@ -111,7 +98,7 @@ int	child2(int fd[2], t_pipex *pipex)
 int	connect(t_pipex *pipex)
 {
 	int		fd[2];
-	int 	pid1;
+	int		pid1;
 	int		pid2;
 
 	if (pipe(fd) == -1)
@@ -121,15 +108,14 @@ int	connect(t_pipex *pipex)
 	}
 	pid1 = child1(fd, pipex);
 	pid2 = child2(fd, pipex);
-	if ( pid1 < 0 || pid2 < 0)
+	if (pid1 < 0 || pid2 < 0)
 	{
 		err_process(fd);
 		return (-1);
 	}
-	close(fd[0]); 
-	close(fd[1]); 
-	// sleep(100);
+	close(fd[0]);
+	close(fd[1]);
 	waitpid(pid1, NULL, 0);
-	waitpid(pid2 , NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (0);
 }
